0ae31bb594d62e38d690f79fc688815d44b38a6b
weicheng112 <cjiyjpgji@gmail.com>
2025-05-23 14:46:31 -0700

    added the backend solver and isValid function

---
+++ solver/src/solver.ts
@@ -0,0 +1,34 @@
+import { sudoku_Board } from "./types.js";
+
+export function solve(board: sudoku_Board): boolean {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (board[row][col] === 0) {
+        for (let num = 1; num <= 9; num++) {
+          if (isValid(board, row, col, num)) {
+            board[row][col] = num;
+            if (solve(board)) return true;
+            board[row][col] = 0;
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function isValid(
+  board: sudoku_Board,
+  row: number,
+  col: number,
+  num: number
+): boolean {
+  for (let i = 0; i < 9; i++) {
+    if (board[row][i] === num || board[i][col] === num) return false;
+    const boxRow = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+    const boxCol = 3 * Math.floor(col / 3) + (i % 3);
+    if (board[boxRow][boxCol] === num) return false;
+  }
+  return true;
+}
